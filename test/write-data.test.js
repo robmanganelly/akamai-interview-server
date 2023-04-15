65c0e9293f4d94b8e3576436ab2b64d99d23f72b
Roberto Manganelly <robmanganelly@gmail.com>
2023-04-15 02:39:36 -0400

    stable test

--- test/write-data.test.js
+++ test/write-data.test.js
@@ -1,8 +1,9 @@
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const path = require('path');
+const { randNames } = require('../utils/rand-words');
 const { unitTests } = require(path.join(__dirname, '..', 'utils', 'write-data.js'));
 
-const { createGroups, giveRandomNetwork } = unitTests;
+const { createAccountIds, createGroups, giveRandomNetwork, prefixAndKey, createAccountGroups } = unitTests;
 
 describe('createGroups', () => {
   it('should create an array of groups with the given names and random networks', () => {
@@ -28,3 +29,100 @@ describe('giveRandomNetwork', () => {
         });
   });
 });
+
+describe('prefixAndKey', () => {
+  // Test case 1
+  it('should return a string', () => {
+    const result = prefixAndKey('John Doe');
+    expect(result).to.be.a('string');
+  });
+
+  // Test case 2
+  it('should return a string with the correct format', () => {
+    const result = prefixAndKey('John Doe');
+    const regex = /^[a-z]+_[a-z0-9]{7}$/; // Regex pattern to match the format
+    expect(result).to.match(regex);
+  });
+
+  // Test case 3
+  it('should return a different result for different input', () => {
+    const result1 = prefixAndKey('John Doe');
+    const result2 = prefixAndKey('Jane Smith');
+    expect(result1).to.not.equal(result2);
+  });
+
+  it('should return a different result for same input', () => {
+    const result1 = prefixAndKey('John Doe');
+    const result2 = prefixAndKey('John Doe');
+    expect(result1).to.not.equal(result2);
+  });
+});
+
+describe('createAccountGroups', () => {
+  it('should return an array with two elements', () => {
+    const result = createAccountGroups(['John Doe', 'Jane Smith']);
+    expect(result).to.be.an('array').with.lengthOf(2);
+  });
+  it('should return an object as the first element of the array', () => {
+    const result = createAccountGroups(['John Doe', 'Jane Smith']);
+    expect(result[0]).to.be.an('object');
+  });
+  it('should return an array of strings as the second element of the array', () => {
+    const result = createAccountGroups(['John Doe', 'Jane Smith']);
+    expect(result[1]).to.be.an('array').of.string;
+  });
+  it('should return an object with the correct structure', () => {
+    const result = createAccountGroups(['John Doe', 'Jane Smith']);
+    const keys = Object.keys(result[0]);
+    expect(keys).to.have.members(['John Doe', 'Jane Smith']);
+    expect(result[0][keys[0]]).to.be.an('array').of.string;
+    expect(result[0][keys[1]]).to.be.an('array').of.string;
+  });
+  it('should return an array of strings with the correct format', () => {
+    const result = createAccountGroups(['John Doe', 'Jane Smith']);
+    const regex = /^[a-z]+_[a-z0-9]{7}$/; // Regex pattern to match the format
+    // expect(result[1]).to.all.match(regex);
+    result[1].forEach((str) => {
+      expect(str).to.match(regex);
+    });
+  });
+});
+
+
+describe('createAccountIds', () => {
+  it('should return an object', () => {
+    const names = ['John', 'Mary', 'David'];
+    const result = createAccountIds(names);
+    expect(result).to.be.an('object');
+  });
+
+  it('should return an object with keys equal to the input names', () => {
+    const names = ['John', 'Mary', 'David'];
+    const result = createAccountIds(names);
+    expect(Object.keys(result)).to.have.members(names);
+  });
+
+  it('should return an object with values in the correct format', () => {
+    const names = ['John', 'Mary', 'David'];
+    const result = createAccountIds(names);
+    const pattern = /^[0-9a-f]{4}-[0-9a-f]{4}$/;
+    Object.values(result).forEach((id) => {
+      expect(id).to.match(pattern);
+    });
+  });
+});
+
+describe('randNames', () => {
+  it('should return a string with two random names separated by a space', () => {
+    const name = randNames();
+    assert.match(name, /^[a-z]+ [a-z]+$/i, 'name format is incorrect');
+  });
+
+  it('should return unique names on each call', () => {
+    const name1 = randNames();
+    const name2 = randNames();
+    console.log(name1, name2);
+    assert.notEqual(name1, name2, 'names are not unique');
+  });
+});
+
--- utils/write-data.js
+++ utils/write-data.js
@@ -1,24 +1,95 @@
+const { randNames } = require("./rand-words");
+
+function performHandling(oldState, times, replace = false) {
+  // create the object
+  // assign properties
+  // if replace, write the file with the new object
+  // else merge with previous object
+  // write the file
+  let newData = Object.create(null);
+  let _accIds = createAccountIds(new Array(times).map(nil=>randNames()));
+  let [_accGr, flatMap] = createAccountGroups(Object.keys(_accIds));//[{},[]]
+  let _groups = createGroups(flatMap);
+
+  if (replace) {
+    newData = { accountIds: _accIds,  accountGroups: _accGr, groups: _groups };
+    return newData;
+  }else{
+    return { 
+      accountIds: {
+      ..._accIds, ...(oldState.accountIds)},
+      accountGroups:{
+      ..._accGr, ...(oldState.accountGroups)
+      },
+      groups:{
+      ..._groups, ...(oldState.groups)}};
+  }
+}
+
+function createAccountIds(names) {
+  return Object.fromEntries(
+    names.map((n) => [
+      n,
+      `${Math.random().toString(16).slice(2, 6)}-${Math.random()
+        .toString(16)
+        .slice(2, 6)}`,
+    ])
+  );
+}
+
+function createAccountGroups(names) {
+  let groups = names.map((name) => [
+    name,
+    new Array(Math.ceil(Math.random() * 10))
+      .fill(1)
+      .map((i) => prefixAndKey(name)),
+  ]);
+  let accGr = Object.fromEntries(groups);
+  return [accGr, groups.map((g) => g[1]).flat()];
+}
+
+/**
+ *
+ * @param {string } name
+ * @returns {string} a random prefix and key
+ */
+function prefixAndKey(name) {
+  return `${name
+    .split(" ")
+    .map((word) => word.slice(0, -1).toLowerCase())
+    .join("")}_${Math.random().toString(36).slice(2, 9)}`;
+}
+
 /**
  * to be used inside createAccountGroups only
  * @param {string[]} names
  * @returns {object[]} an array of groups (name and networks)
  */
 function createGroups(names) {
-    return names.map((name) => ({ name, networks: giveRandomNetwork()}));
-  }
-  
-  /**
-   * to be used inside createGroups only
-   * @returns {string} a random networks[]
-   */
-  function giveRandomNetwork() {
-    let arrBase = Math.floor(Math.random() * 10);
-    return new Array(arrBase > 0 ? arrBase : 1).fill(1).map(
-      ip=>`${Math.floor(Math.random()*255)}.${Math.floor(Math.random()*255)}.${Math.floor(Math.random()*255)}.${Math.floor(Math.random()*255)}`
+  return names.map((name) => ({ name, networks: giveRandomNetwork() }));
+}
+
+/**
+ * to be used inside createGroups only
+ * @returns {string} a random networks[]
+ */
+function giveRandomNetwork() {
+  return new Array(Math.ceil(Math.random() * 10))
+    .fill(1)
+    .map(
+      (ip) =>
+        `${Math.floor(Math.random() * 255)}.${Math.floor(
+          Math.random() * 255
+        )}.${Math.floor(Math.random() * 255)}.${Math.floor(
+          Math.random() * 255
+        )}`
     );
-  }
+}
 
-
-  module.exports.unitTests = {
-    createGroups,giveRandomNetwork
-  }
+module.exports.unitTests = {
+  createGroups,
+  giveRandomNetwork,
+  prefixAndKey,
+  createAccountGroups,
+  createAccountIds,
+};
