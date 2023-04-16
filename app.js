c93d418c1622c552278f70e6b7fe6ec2aa8c6614
Roberto Manganelly <robmanganelly@gmail.com>
2023-04-16 02:12:47 -0400

    added cors in server

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var router = require('./routes');
 var router2 = require('./routes/v2');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
